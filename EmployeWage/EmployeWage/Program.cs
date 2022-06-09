d7f3998b579756ef958e92e45f009f9887aeb14b
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-09 23:39:44 +0530

    Check Wether employee present or absent

---
+++ EmployeWage/EmployeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
