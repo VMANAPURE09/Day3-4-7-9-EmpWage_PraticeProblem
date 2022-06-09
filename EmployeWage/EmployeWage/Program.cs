c9a03c92910652eb933d44573d9d8b310e43d62d
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-09 23:52:12 +0530

    Calculate Daily Wage

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -6,7 +6,29 @@ namespace EmployeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // UC1 present or absent
+            // UC2 Calculate Daily Wage
+            CheckEmployeePresentAbsent();
+        }
+        public static void CheckEmployeePresentAbsent()
+        {
+            Console.WriteLine("Welcome to Employee Pay Roll");
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.WriteLine("Total Daily wage is: " + dailyWage);
+
         }
     }
 }
