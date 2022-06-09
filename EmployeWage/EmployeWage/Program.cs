4d3fd78df6ec9207d0a9c253f20408b3384cde26
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-09 23:59:18 +0530

    Add Part Time Employee & Wage

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -8,20 +8,29 @@ namespace EmployeWage
         {
             // UC1 present or absent
             // UC2 Calculate Daily Wage
+            // UC3 Part time Employee
+            
             CheckEmployeePresentAbsent();
         }
         public static void CheckEmployeePresentAbsent()
         {
             Console.WriteLine("Welcome to Employee Pay Roll");
             int totalWorkingHour = 8;
+            int partTimeHour = 4;
             int wagePerHour = 20;
             int dailyWage = 0;
             Random random = new Random();
-            int num = random.Next(0, 2);
+            int num = random.Next(0, 3);
             if (num == 0)
             {
                 Console.WriteLine("Employee is Present");
                 dailyWage = totalWorkingHour * wagePerHour;
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("Employee is doing Parttime");
+                dailyWage = partTimeHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
             }
             else
             {
