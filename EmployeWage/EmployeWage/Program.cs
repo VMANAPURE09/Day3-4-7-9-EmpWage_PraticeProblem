15dc318afc61d68df2789d3dbd5bda6a83c94350
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-12 00:45:53 +0530

    Calculate wage for month

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -15,28 +15,39 @@ namespace EmployeWage
         public static void CheckEmployeePresentAbsent()
         {
             Console.WriteLine("Welcome to Employee Pay Roll");
-            int totalWorkingHour = 8;
-            int partTimeHour = 4;
-            int wagePerHour = 20;
-            int dailyWage = 0;
-            Random random = new Random();
-            int num = random.Next(0, 3);
-            switch (num)
+            const int Emp_Full_Time = 1;
+            const int Emp_Part_Time = 2;
+        public int Working_Days_Per_Month = 20;
+        public int Total_Salary_Month = 0;
+        public int Day;
+        public int empHrs = 0;
+        public int EmployeeWage;
+        public void Attendance()
+        {
+            // Stroing RandomValue
+            for (Day = 1; Day <= Working_Days_Per_Month; Day++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Present");
-                    dailyWage = totalWorkingHour * wagePerHour;
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is doing Parttime");
-                    dailyWage = partTimeHour * wagePerHour;
-                    Console.WriteLine("Total Daily wage is: " + dailyWage);
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                Random random = new Random(); //Random Value
+                int empCheck = random.Next(0, 3); //Check Random in b/w 0 to 3
+                switch (empCheck) // switch Case
+                {
+                    case Emp_Full_Time: // For Full Time =1
+                        empHrs = 8; // *8
+                        break;
+
+                    case Emp_Part_Time: //for Part Time = 2
+                        empHrs = 4; //*4
+                        break;
+
+                    default:
+                        empHrs = 0; //Default 0
+                        break;
+                }
             }
-            Console.WriteLine("Total Daily wage is: " + dailyWage);
+            //calculating 
+            EmployeeWage = empHrs * Working_Days_Per_Month;
+            Total_Salary_Month += EmployeeWage;  // Total_Salary_Month = Total_Salary_Month + EmployeeWage
+            Console.WriteLine("Employee Wage : " + EmployeeWage); // op
 
         }
     }
