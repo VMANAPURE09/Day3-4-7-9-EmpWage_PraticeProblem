e92c9e12be6a25c49edede8a48e7c2dfd35eaf48
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-20 16:37:05 +0530

    UC8 for Multiple Company Added

--- EmployeWage/EmployeWage/EmployeeWage.cs
+++ EmployeWage/EmployeWage/EmployeeWage.cs
@@ -10,43 +10,58 @@ namespace EmployeWage
     {
         public static void CheckEmployeePresentAbsent()
         {
-            Console.WriteLine("Welcome to Employee Pay Roll");
-            int Emp_Rate_Per_Hour = 20;
-            int Emphrs = 0;
-            int Empwage = 0;
-            int total_Emp_hrs = 0;
-            int total_working_days = 0;
-
-            int Total_working_hrs = 100;
-            int Total_working_days = 20;
-            int Totalwage = 0;
-            while (total_Emp_hrs < Total_working_hrs && total_working_days < Total_working_days)
+            const int Emp_Full_Time = 1;
+            const int Emp_Part_Time = 2;
+        // public int Count_Work_Day = 20;
+        // public int Emp_Per_Rate_Hour = 20;
+        //public int MAx_Hrs_Or_Day_Month= 100;
+        public int Working_Days_Per_Month = 20;
+        public int Working_Days = 0;
+        public int empHrs = 0;
+        public int EmployeeWages = 0;
+        int totalWage = 0;
+        int totalHours = 0;
+        public void Attendance(string CompanyName, int WagePerHour, int NumWorkingDays, int MaxHoursPerMonth) //
+        {
+            // Stroing RandomValue
+            for (int Day = 1; Day <= Working_Days_Per_Month; Day++)
             {
-                total_working_days++;
-                Random random = new Random();
-                int employeecheck = random.Next(2);
+                Random random = new Random(); //Random Value
 
-                if (employeecheck == 1)
+                int empCheck = random.Next(0, 3); //Check Random in b/w 0 to 3
+                switch (empCheck) // switch Case
                 {
-                    Console.WriteLine("Employee is Present");
-                    Emphrs = 8;
+                    case Emp_Full_Time: // For Full Time =1
+                        empHrs = WagePerHour * 8; // *8
+                        break;
+
+                    case Emp_Part_Time: //for Part Time = 2
+                        empHrs = WagePerHour * 4; //*4
+                        break;
+
+                    default:
+                        empHrs = 0; //Default 0
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Emplyee is Absent");
-                    Emphrs = 0;
-                }
 
-
-
-                total_Emp_hrs = total_Emp_hrs + Emphrs;
-
-                Empwage = total_Emp_hrs * Emp_Rate_Per_Hour;
-
-
-
-                Console.WriteLine("Total Employee Wage :" + Empwage);
+                // EmployeeWage += empHrs; // EmployeeWage = EmployeeWage+ empHrs
+                //Console.WriteLine("Day#:" + Working_Days + "Employee Hrs:" + empHrs);
+                //checking total number of hours
+                totalHours += empHrs; //calculate total hours worked
+                totalWage += empHrs; // calculating montly wage
+                if (totalHours >= MaxHoursPerMonth) //maximum total hours 
+                    break;
             }
+
+            //EmpSalary = EmployeeWage + Emp_Per_Rate_Hour; // EmpSalary = EmployeeWage + Emp_Per_Rate_Hour
+            // Console.WriteLine("Employee Wage : " + EmployeeWage); // op
+            Console.WriteLine();
+            Console.WriteLine("Company Name :" + CompanyName);
+            Console.WriteLine("Number Of Hour Employee  Worked  :" + totalHours);
+            Console.WriteLine("Employee Wage Per Hour :" + WagePerHour);
+            Console.WriteLine("Monthly wage :" + totalWage);
+            Console.WriteLine();
         }
+      }
     }
 }
